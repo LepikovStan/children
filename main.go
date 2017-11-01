8fa9d7451dfb81c78e10a4d7c468566542895832
Stanislav Lepikov <Stanislav Lepikov>
2017-11-02 00:29:20 +0300

    task

---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"os"
+	"bufio"
+	"time"
+	"fmt"
+	"flag"
+	"strings"
+)
+
+func readFile(path string) []string {
+	var result []string
+	inFile, _ := os.Open(path)
+	defer inFile.Close()
+	scanner := bufio.NewScanner(inFile)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		result = append(result, scanner.Text())
+	}
+	return result
+}
+
+var d bool
+func initFlags() {
+	flag.BoolVar(&d, "d", false, "")
+	flag.Parse()
+}
+
+func hasThreeVowels(name string) bool {
+	vowels := "aeuoi"
+	splittedName := strings.Split(name, "")
+	vowelsCount := 0
+
+	for _, char := range(splittedName) {
+		if strings.Contains(vowels, char) {
+			vowelsCount++
+		}
+	}
+	return vowelsCount > 2
+}
+
+func hasWrongSubString(name string) bool {
+	wrongStrings := []string{"ab", "cd", "pq", "xy"}
+
+	for _, str := range(wrongStrings) {
+		if strings.Contains(name, str) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func hasDuplicateChar(name string) bool {
+	chars := strings.Split(name, "")
+	charsLen := len(chars)
+	for i := 1; i < charsLen; i++ {
+		if chars[i] == chars[i-1] {
+			return true
+		}
+	}
+	return false
+}
+
+func simple() {
+	names := readFile("input.txt")
+	goodNamesCount := 0
+	for _, name := range(names) {
+		if !hasWrongSubString(name) && hasThreeVowels(name) && hasDuplicateChar(name) {
+			goodNamesCount++
+		}
+	}
+	fmt.Println("Result = ", goodNamesCount)
+}
+func difficult() {}
+
+func main() {
+	start := time.Now()
+	initFlags()
+	if d {
+		difficult()
+	} else {
+		simple()
+	}
+	end := time.Now()
+	fmt.Println(end.Sub(start))
+}
